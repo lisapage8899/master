7d2f96ba496fedf715c265093aa30783e11d4d5a
G T <lisa.page8899@gmail.com>
2019-10-29 19:50:24 -0700

    Adding teacup images, features, steps example for cucumber testing.

---
+++ teashop/steps/maths.js
@@ -0,0 +1,17 @@
+// steps/maths.js
+const defineSupportCode = require('cucumber').defineSupportCode;
+const assert = require('assert');
+
+defineSupportCode(
+  function({ Given, Then, When }){
+    let answer = 0;
+    Given('I start with {int}', function(input) {
+        answer = input;
+    });
+    When('I add {int}', function(input){
+        answer = answer + input;
+    });
+    Then('I end up with {int}', function(input){
+        assert.equal(answer, input);
+    });
+});
